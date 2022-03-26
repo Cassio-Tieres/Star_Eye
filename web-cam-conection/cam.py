cd70c55d5ecd332c25c97a38d3a99e880f2b9d6a
Cássio Tieres Bomfim <tieresbomfim@gmail.com>
2022-03-26 02:11:41 -0300

    Primeiro Commit - conexao da web cam

---
+++ web-cam-conection/cam.py
@@ -0,0 +1,18 @@
+import cv2
+
+webcam = cv2.VideoCapture(0)
+
+if webcam.isOpened():
+    validacao, frame = webcam.read()
+    while validacao:
+        validacao, frame = webcam.read()
+        cv2.imshow("Vídeo da webcam garaio", frame)
+        key = cv2.waitKey(5)
+
+        if key == 27: #refere-se a tecla 27
+            break
+
+    cv2.imwrite("photograpy.jpg", frame)
+
+webcam.release()
+cv2.destroyAllWindows()
