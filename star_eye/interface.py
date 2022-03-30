4c80289095c123d95f9d5369278ec25a143bcb29
Cássio Tieres Bomfim <tieresbomfim@gmail.com>
2022-03-29 21:37:39 -0300

    Adicionando um icon.ico a janela e um separador no body

--- star_eye/interface.py
+++ star_eye/interface.py
@@ -1,5 +1,7 @@
 from cgitb import text
 from email.mime import image
+from re import X
+from textwrap import fill
 from tkinter import font
 from turtle import left, window_height, window_width
 import cam 
@@ -9,9 +11,10 @@ from tkinter import ttk
 window = tk.Tk()
 
 window.title('Star Eye')
+window.iconbitmap('assets/Icon.ico')
 
 window_width = 1000
-window_height = 630
+window_height = 450
 
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
@@ -24,20 +27,20 @@ window.resizable(False, False)
 
 t_icon = tk.PhotoImage(file='assets/Icon.png')
 
-title = ttk.Label(
+title = tk.Label(
     window,
     text='Star Eye',
-    font=('Sail', 20),
+    font=('Asar', 20),
     image=t_icon,
     compound=tk.RIGHT
 ).pack()
 
+separator = ttk.Separator(window, orient='horizontal').pack(fill='x')
 
 b_iniciarObservacao = ttk.Button(
     window,
     text='Iniciar observação',
     compound=tk.LEFT,
-    
 ).pack(ipadx=20, ipady=10, expand=True)
 
 window.mainloop()
