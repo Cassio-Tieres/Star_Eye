b038e8354f9a2c9e40ef80f7e8ea6a02f3a9b50a
Cássio Tieres Bomfim <tieresbomfim@gmail.com>
2022-03-29 21:11:34 -0300

    Centralização da janela na tela :)

--- star_eye/interface.py
+++ star_eye/interface.py
@@ -1,13 +1,25 @@
 from cgitb import text
 from email.mime import image
 from tkinter import font
-from turtle import left
+from turtle import left, window_height, window_width
 import cam 
 import tkinter as tk
 from tkinter import ttk
 
 window = tk.Tk()
-window.geometry('1000x653')
+
+window.title('Star Eye')
+
+window_width = 1000
+window_height = 630
+
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+center_x = int(screen_width/2 - window_width / 2)
+center_y = int(screen_height/2 - window_height / 2)
+
+window.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 window.resizable(False, False)
 
 t_icon = tk.PhotoImage(file='assets/Icon.png')
@@ -17,16 +29,15 @@ title = ttk.Label(
     text='Star Eye',
     font=('Sail', 20),
     image=t_icon,
-    compound=tk.LEFT
+    compound=tk.RIGHT
 ).pack()
 
 
 b_iniciarObservacao = ttk.Button(
     window,
     text='Iniciar observação',
-    width=30,
     compound=tk.LEFT,
     
-).pack()
+).pack(ipadx=20, ipady=10, expand=True)
 
 window.mainloop()
